5aeee9a96ce1c1d9f6c42d91aed4d3b5b37cc318
YiWei Zheng <yiweizhe@usc.edu>
2012-12-20 02:00:41 -0800

    astyle

--- KerbalShip.php
+++ KerbalShip.php
@@ -28,24 +28,23 @@ class KerbalShip {
     }
     public function loadMeta(&$string) {
         $arr = explode("\n",$string);
-  //      var_dump($arr[0]);
+        //      var_dump($arr[0]);
         preg_match('/(\w+) = ([\w\W]+)/',$arr[0],$match);
-$this->ship = $match[2];
-preg_match('/(\w+) = ([\.\d]+)/',$arr[1],$match);
+        $this->ship = $match[2];
+        preg_match('/(\w+) = ([\.\d]+)/',$arr[1],$match);
 //var_dump($match);
-$this->version = $match[2];
-preg_match('/(\w+) = ([\w]+)/',$arr[2],$match);
+        $this->version = $match[2];
+        preg_match('/(\w+) = ([\w]+)/',$arr[2],$match);
 //var_dump($match);
-$this->type = $match[2];
-echo $this;
+        $this->type = $match[2];
+        echo $this;
         for($i = 0; $i != 3; $i++)
             unset($arr[0]);
         return implode("\n",$arr);
     }
-public function __toString()
-{
-return $this->ship.' '.$this->version.' '.$this->type;
-}
+    public function __toString() {
+        return $this->ship.' '.$this->version.' '.$this->type;
+    }
 }
 
 ?>
